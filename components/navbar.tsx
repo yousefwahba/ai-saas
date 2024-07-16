85f95f6f3045b073209a6c76d7eb8b04bfc5b668
yousefwahba <yousefwahba.official@gmail.com>
2024-07-16 17:22:42 +0300

    added-mobile-sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import { Menu } from "lucide-react";
+import MobileSidebar from "./mobile-sidebar";
 
 const Navbar = () => {
-    return ( 
+  return (
     <div className="flex items-center p-4">
-        <Button variant={"ghost"} size={"icon"} className="md:hidden">
-            <Menu />
-        </Button>
-        <div className="flex w-full justify-end">
-            <UserButton />
-        </div>
-    </div> 
-    );
-}
- 
+      <MobileSidebar />
+      <div className="flex w-full justify-end">
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -66,7 +66,7 @@ const routes = [
 
 const Sidebar = () => {
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full bg-[#11827] text-white">
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
         <Link href={"/dashboard"} className="flex items-center pl-3 mb-14">
           <div className="h-8 w-8 relative mr-4">
