7d592455760f5c59cdd04f64fb37d23d1bd30766
yousefwahba <yousefwahba.official@gmail.com>
2024-08-10 22:41:59 +0300

    added-crisp-customer-support

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModelProvider } from "@/components/model-provider";
 import { ToasterProvider } from "@/components/toaster-provider";
+import { CrispProvider } from "@/components/crisp-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider afterSignOutUrl={"/"}>
       <html lang="en">
+        <CrispProvider />
         <body className={inter.className}>
           <ModelProvider />
           <ToasterProvider />
